4690fda4faa8242c4334955ca4f33cb0af61fb7f
Vano <whoasked@tutanota.com>
2024-03-24 00:42:42 +0200

    callable_lambda test

--- project/Example.gd
+++ project/Example.gd
@@ -1,4 +1,12 @@
 extends Example
 
 func _ready():
-	do_something()
+	exposed_method()
+	
+	print(get_preload())
+	$Sprite2D.texture = get_preload()
+	get_node("2356362")
+	var a = [1]
+	get_ref(a)
+	
+	#get_tree().create_timer(5).timeout.connect(get_tree().quit)
